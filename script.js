a3ab7433605ca37e65b446a0771550e5b817dc84
fmilan1 <fazekas.milan1@gmail.com>
2021-09-28 20:06:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+﻿window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    let menubox = document.getElementById("menubox");
+    if (document.documentElement.scrollTop > 30) {
+        menubox.style.top = "4%";
+        menubox.style.width = "20%";
+        for (let i = 0; i < menubox.children.length; i++)
+        {
+            menubox.children[i].style.fontSize = "44%";
+        }
+    } else {
+        menubox.style.top = "10%";
+        menubox.style.width = "60%";
+        for (let i = 0; i < menubox.children.length; i++)
+        {
+            menubox.children[i].style.fontSize = "100%";
+        }
+    }
+}
+
+
+function felnagyit()
+{
+    let menubox = document.getElementById("menubox");
+    menubox.style.width = "60%";
+    for (let i = 0; i < menubox.children.length; i++)
+    {
+        menubox.children[i].style.fontSize = "100%";
+    }
+}
+
+function kicsinyit()
+{
+    if (document.documentElement.scrollTop > 30) {
+        let menubox = document.getElementById("menubox");
+        menubox.style.width = "20%";
+        for (let i = 0; i < menubox.children.length; i++)
+        {
+            menubox.children[i].style.fontSize = "44%";
+        }
+    }
+}
